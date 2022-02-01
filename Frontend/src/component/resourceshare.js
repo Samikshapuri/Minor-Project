c936e5bcd1d8e896acaadf86b8d6c70bedf14396
megha <meghagupta190801@gmail.com>
2022-02-01 20:37:12 +0530

    Job ongoing

--- Frontend/src/component/resourceshare.js
+++ Frontend/src/component/resourceshare.js
@@ -1,51 +1,29 @@
 import { Col, Container, Row, Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { Modal,  Button } from "react-bootstrap";
+import { Modal, Button} from "react-bootstrap";
 import React from "react";
 import { useState } from "react";
 
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-
-function resourceshare() {
+function Resourceshare() {
   const [modalShow, setModalShow] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const toggleShow = () => setShow((s) => !s);
+
   return (
     <div>
       <React.Fragment>
-        <div className="account-home-btn d-none d-sm-block">
-          <Link to="/" className="text-primary">
-            <i className="mdi mdi-home h1"></i>
-          </Link>
-        </div>
-
         <Container fluid>
           <Row>
             <Col className="resource-nav" lg={2}>
+              <Row>
+                <div className="account-home-btn d-none d-sm-block">
+                  <Link to="/" className="text-primary">
+                    <i className="mdi mdi-home h1"></i>
+                  </Link>
+                </div>
+              </Row>
               <Navbar collapseOnSelect expand="lg" variant="light" fixed="left">
                 <Container className="resource-nav-column">
                   <Navbar.Brand href="#home">Resource-Sharing</Navbar.Brand>
@@ -60,25 +38,64 @@ function resourceshare() {
                         onClick={() => setModalShow(true)}
                       >
                         &nbsp; Offers &nbsp;
-                        <MyVerticallyCenteredModal
-                          show={modalShow}
-                          onHide={() => setModalShow(false)}
-                        />
                       </Nav.Link>
-                      <Nav.Link href="#requests">Requests</Nav.Link>
+                      <Nav.Link
+                        href="#requests"
+                        onClick={() => setModalShow(true)}
+                      >
+                        Requests
+                      </Nav.Link>
                       <Nav.Link href="#map">Map</Nav.Link>
                     </Nav>
                   </Navbar.Collapse>
                 </Container>
               </Navbar>
             </Col>
-            <Col lg={6}></Col>
-            <Col lg={4}></Col>
+            <Col>
+            <div className="account-home-btn d-none d-sm-block" onClick={toggleShow}>                  
+                    <i className="mdi mdi-menu h1"></i>                  
+                </div>
+            </Col>
+            <Col show={show} onHide={handleClose} lg={3}>
+            </Col>
           </Row>
         </Container>
       </React.Fragment>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </div>
   );
 }
 
-export default resourceshare;
+export default Resourceshare;
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Make an offer / request
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Post</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+        <Button>Post</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
