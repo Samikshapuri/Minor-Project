47cb79ccd6cd7b167e432b390cea0bc1edaf93db
megha <meghagupta190801@gmail.com>
2022-02-08 17:40:00 +0530

    maps added

--- Frontend/src/component/Map.js
+++ Frontend/src/component/Map.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {MapContainer, TileLayer, Marker,Popup } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 
 function Map(props) {
   return <div>
-      <MapContainer className="map" center={[2.5, 112.5]} zoom={3} scrollWheelZoom={false}>
+      <MapContainer className="map" center={[2.5, 112.5]} zoom={3} scrollWheelZoom={false} style={{ height: '100vh', width: '100wh' }}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -28,40 +29,3 @@ function Map(props) {
 }
 
 export default Map;
-
-
-
-
-
-
-
-// //jshint esversion:6
-// import {MapContainer, TileLayer, Marker,Popup } from 'react-leaflet';
-
-// function Map(props) {
-//     return(
-//         <MapContainer className="map" center={[2.5, 112.5]} zoom={3} scrollWheelZoom={false}>
-//             <TileLayer
-//                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-//                 opacity={20}
-//             />
-//             {props.data.map((statistic, index) => {
-//                 const updated = new Date(statistic.updated);
-//                 return (
-//                     <Marker key={index} position={[statistic.countryInfo.lat, statistic.countryInfo.long]}>
-//                         <Popup>
-//                             <h2><img src= {statistic.countryInfo.flag} height="20px" width="25px"/> {statistic.country}</h2>
-//                              <strong>Confirmed:</strong> {statistic.cases.toLocaleString()}<br/>
-//                              <strong>Death:</strong> {statistic.deaths.toLocaleString()}<br/>
-//                              <strong>Recovered:</strong> {statistic.recovered.toLocaleString()}<br/>
-//                              <strong>Last Update:</strong> {updated.getDate()}/{updated.getMonth() + 1}/{updated.getFullYear()} {updated.getHours()}:{updated.getMinutes()}
-//                         </Popup>
-//                     </Marker>
-//                 )
-//             })}
-//         </MapContainer>
-//     )
-// }
-
-// export default Map;
---
+++ Frontend/src/component/MapResources.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Circle,
+  CircleMarker,
+  MapContainer,
+  Popup,
+  TileLayer,
+  Marker, } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useCallback, useMemo, useRef, useState } from 'react';
+
+
+const center = [51.505, -0.09]
+const fillBlueOptions = { fillColor: 'blue' }
+const redOptions = { color: 'red' }
+
+function DraggableMarker() {
+  const [draggable, setDraggable] = useState(false)
+  const [position, setPosition] = useState(center)
+  const markerRef = useRef(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current
+        if (marker != null) {
+          setPosition(marker.getLatLng())
+        }
+      },
+    }),
+    [],
+  )
+  const toggleDraggable = useCallback(() => {
+    setDraggable((d) => !d)
+  }, [])
+
+  return (
+    <Marker
+      draggable={draggable}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}>
+      <Popup minWidth={90}>
+        <span onClick={toggleDraggable}>
+          {draggable
+            ? 'Marker is draggable'
+            : 'Click here to make marker draggable'}
+        </span>
+      </Popup>
+    </Marker>
+  )
+}
+
+
+function MapResources() {
+  return <div>
+    <MapContainer center={[51.505, -0.09]} zoom={15} style={{ height: '600px', width: '600px' }}>
+  <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Circle center={center} pathOptions={fillBlueOptions} radius={200} />
+      <CircleMarker
+        center={[51.51, -0.12]}
+        pathOptions={redOptions}
+        radius={20}>
+        <Popup>Popup in CircleMarker</Popup>
+      </CircleMarker>   
+      
+      <DraggableMarker />
+</MapContainer>
+  </div>;
+}
+
+export default MapResources;
